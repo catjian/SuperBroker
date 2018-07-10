22ce8c35b2546adb42213dda405acac3d3da775a
zhangjian <zhangjian@eavchina.com>
2018-07-10 20:49:23 +0800

    commit clean project

---
+++ SuperBroker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperBroker
+//
+//  Created by zhang_jian on 2018/7/10.
+//  Copyright © 2018年 zhangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperBroker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperBroker
+//
+//  Created by zhang_jian on 2018/7/10.
+//  Copyright © 2018年 zhangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
